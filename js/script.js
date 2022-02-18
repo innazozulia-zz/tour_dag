0520cd5db9192094f024fc7994fa0ebe9f00426a
innazozulia <innazozulia@outlook.com>
2022-02-18 13:50:45 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+var swiper = new Swiper('.day-one', {
+   grabCursor:true,
+   direction: 'horizontal',
+   loop: true,
+   slidesPerView: 1,
+   spaceBetween: 10,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+var swiper = new Swiper('.day-two', {
+   grabCursor:true,
+   direction: 'horizontal',
+   loop: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+ var swiper = new Swiper('.day-three', {
+   grabCursor:true,
+   direction: 'horizontal',
+   loop: true,
+ 
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+     
+    },
+   
+ 
+ });
+ var swiper = new Swiper('.day-four', {
+   grabCursor:true,
+   direction: 'horizontal',
+   loop: true,
+ 
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ 
+ });
+ var swiper = new Swiper('.day-five', {
+   grabCursor:true,
+   direction: 'horizontal',
+   loop: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+ 
